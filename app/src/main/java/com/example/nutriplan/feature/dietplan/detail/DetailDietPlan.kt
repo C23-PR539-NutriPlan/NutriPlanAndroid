28d57b88dce95e965c20bb7b61eb08f924ddbbed
Elroi Yoshua <elroiyoshua@student.telkomuniversity.ac.id>
2023-06-12 11:47:10 +0700

    Changing layout for foodRecomm

--- app/src/main/java/com/example/nutriplan/feature/dietplan/detail/DetailDietPlan.kt
+++ app/src/main/java/com/example/nutriplan/feature/dietplan/detail/DetailDietPlan.kt
@@ -20,51 +20,13 @@ class DetailDietPlan : AppCompatActivity() {
 
         binding = ActivityDetailDietPlanBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val dataPlan = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            intent.getParcelableExtra( KEY_PLAN, Plan::class.java)
-        } else {
-            @Suppress("DEPRECATION")
-            intent.getParcelableExtra(KEY_PLAN)
-        }
-
-        if (dataPlan != null){
-            binding.apply {
-                judulFood.text = dataPlan.name
-                maxCalDetail.text = dataPlan.max_cal
-                gambarMakanan.setImageResource(dataPlan.photo)
-            }
-            binding.rvDetailDietPlan.setHasFixedSize(true)
-            if (binding.rvDetailDietPlan.size == 0){
-                listPlan.addAll(getlistPlan())
-            }
-        }
-        showListPlan()
         supportActionBar?.hide()
     }
 
-    private fun getlistPlan():ArrayList<Plan>{
-        val dataName = resources.getStringArray(R.array.name)
-        val dataMax_Cal = resources.getStringArray(R.array.Max_Cal)
-        val dataIngredients = resources.getStringArray(R.array.Ingredients)
-        val photo = resources.obtainTypedArray(R.array.Photo)
-        val listPLAN = ArrayList<Plan>()
-
-        for(i in dataName.indices){
-            val PLAN = Plan(dataName[i],dataIngredients[i],dataMax_Cal[i], photo.getResourceId(i,-1))
-            listPLAN.add(PLAN)
-        }
-        return listPLAN
-    }
 
 
-    private fun showListPlan(){
-        for (i in  0 .. 9){
-            binding.rvDetailDietPlan.layoutManager = LinearLayoutManager(this)
-            val list = DetailDietPlanAdapter(listPlan)
-            binding.rvDetailDietPlan.adapter = list
-        }
-    }
+
+
 
     companion object{
         const val KEY_PLAN = "key_Plan"

e4039464c4a1ba8f55f361ed55f81e200cce3fb1
Elroi Yoshua <elroiyoshua@student.telkomuniversity.ac.id>
2023-06-15 09:44:18 +0700

    Splash

--- app/src/main/java/com/example/nutriplan/SplashScreen.kt
+++ app/src/main/java/com/example/nutriplan/SplashScreen.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nutriplan.feature.login.login
 
-class MainActivity : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
